93cdf05d51c704d50a48385edcde42bbe4ea53c6
Deju Adejo <ojomideju2003@gmail.com>
2024-06-21 14:52:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+const fs = require('node:fs');
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
 /* 
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
